b9f783457f945636ae4cce0ea5bf855d561b6b72
httpmurilo <muriloeduardo1997@outlook.com>
2024-06-07 15:38:43 -0300

    projeto

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import http from "http";
 
 const PORT = 3000;
 
+const ROTAS = {
+    "/" : "Curso de nodeJS",
+    "/livros" : "",
+    "/autores" : ""
+};
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type" : "text/plain"});
-    res.end("Curso de node");
+    res.end(rotas[req.url]);
 });
 
 server.listen(PORT, () => {
